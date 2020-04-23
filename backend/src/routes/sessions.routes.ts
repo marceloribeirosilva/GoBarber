c44e058b47cbce172a66067c1057977fce70f5cf
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-04-23 16:02:23 -0300

    validando credenciais

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AuthenticateUseService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUseService();
+
+    const { user } = await authenticateUser.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
