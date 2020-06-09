2388e389b9cab25cd70308680f1c126e3457601b
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-06-09 11:53:28 -0300

    Configurando Rotas e Rotas autenticadas

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>Página Dashboard</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
