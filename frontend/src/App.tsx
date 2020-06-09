f1d178c6d4089b5a97c3cc0bf13c76e6283a8bfe
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-06-08 22:19:12 -0300

    login pelo contexto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,18 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-import AuthContext from './hooks/auth';
+import { AuthProvider } from './hooks/auth';
 
-const App: React.FC = () => (
-  <>
-    <AuthContext.Provider value={{ name: 'Marcelo' }}>
-      <SignIn />
-    </AuthContext.Provider>
+const App: React.FC = () => {
+  return (
+    <>
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
 
-    <GlobalStyle />
-  </>
-);
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
