417b2dc916947b8092334a4fbdc0815ab74db409
marceloribeirosilva <ribmarc@gmail.com>
2020-07-10 11:21:35 -0300

    refactor module appointments

---
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,8 @@
+import Appointments from '../infra/typeorm/entities/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointments | undefined>;
+}
